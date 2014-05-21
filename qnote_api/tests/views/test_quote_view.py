abd465f097691ba2d66af0d20cff41842b325656
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-05-22 01:02:39 +0700

    [r] Updated test with test data

--- qnote_api/tests/views/test_quote_view.py
+++ qnote_api/tests/views/test_quote_view.py
@@ -6,12 +6,18 @@ import sure
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
+from common.tests.factories.user_factory import UserFactory
 from qnote_api.models.quote import Quote
+from qnote_api.tests.factories.quote_factory import QuoteFactory
 
 
 class QuoteViewTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user = UserFactory()
+
+        for i in range(0, 10):
+            QuoteFactory(user=self.user)
 
     def test_get_all_quotes(self):
         response = self.client.get(reverse('qnote_api_quotes'))
