e8356ebe3c07be71c9f880729b3fb1fcb110e284
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-05-22 00:35:10 +0700

    Added fields to Quote

--- qnote_api/models/quote.py
+++ qnote_api/models/quote.py
@@ -1,10 +1,16 @@
 '''
 Created by tklarryonline on May 21, 2014.
 '''
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
 class Quote(models.Model):
+    user = models.ForeignKey(User)
+    saying = models.TextField(_("Saying"))
+    made_at = models.DateTimeField(_("Made at"))
+    added_at = models.DateTimeField(_("Added at"))
 
     class Meta:
         app_label = "qnote_api"
